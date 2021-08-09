ec4063980c7bfb8e509f03fad0458aa1091a6592
karinagiuseppina <karina.giuseppina@gmail.com>
2021-08-09 19:39:29 +0000

    done

--- src/app.js
+++ src/app.js
@@ -2,10 +2,45 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  createNewCard();
 };
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["♦", "♥", "♠", "♣"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
+
+document
+  .querySelector(".button-generate")
+  .addEventListener("click", createNewCard);
+
+function createNewCard() {
+  let card = document.querySelector(".card");
+  let top = card.querySelector(".top-suit");
+  let bottom = card.querySelector(".bottom-suit");
+  let number = card.querySelector(".number");
+  top.innerHTML = bottom.innerHTML = generateRandomSuit();
+  number.innerHTML = generateRandomNumber();
+}
